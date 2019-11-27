e47597e895b4779cf0d5efdf735b314545acca2d
Alex Brui <javasabr@gmail.com>
2019-11-27 06:18:19 +0300

    [broker-22] reduce code duplication in packet handlers

--- src/main/java/com/ss/mqtt/broker/handler/packet/in/PublishCompleteInPacketHandler.java
+++ src/main/java/com/ss/mqtt/broker/handler/packet/in/PublishCompleteInPacketHandler.java
@@ -1,19 +1,7 @@
 package com.ss.mqtt.broker.handler.packet.in;
 
-import com.ss.mqtt.broker.network.client.MqttClient.UnsafeMqttClient;
-import com.ss.mqtt.broker.network.packet.in.PublishAckInPacket;
 import com.ss.mqtt.broker.network.packet.in.PublishCompleteInPacket;
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
-public class PublishCompleteInPacketHandler extends AbstractPacketHandler<UnsafeMqttClient, PublishCompleteInPacket> {
-
-    @Override
-    protected void handleImpl(@NotNull UnsafeMqttClient client, @NotNull PublishCompleteInPacket packet) {
-        var session = client.getSession();
-        if (session != null) {
-            session.updateOutPendingPacket(client, packet);
-        }
-    }
-}
+public class PublishCompleteInPacketHandler extends PendingOutResponseInPacketHandler<PublishCompleteInPacket> {}
