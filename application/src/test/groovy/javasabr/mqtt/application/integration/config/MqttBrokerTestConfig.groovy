c8e48d15f9122b266f0731235bc09aebaed8bbab
javasabr <javasabr@gmail.com>
2025-09-28 16:15:27 +0200

    move legacy part to application

---
+++ application/src/test/groovy/javasabr/mqtt/application/integration/config/MqttBrokerTestConfig.groovy
@@ -0,0 +1,40 @@
+package javasabr.mqtt.application.integration.config
+
+import javasabr.mqtt.application.config.MqttBrokerConfig
+import javasabr.mqtt.application.config.MqttNetworkConfig
+import javasabr.mqtt.network.MqttConnection
+import javasabr.rlib.network.server.ServerNetwork
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.PropertySource
+
+import java.util.function.Consumer
+
+@Import([
+    MqttBrokerConfig,
+    MqttNetworkConfig
+])
+@Configuration(proxyBeanMethods = false)
+@PropertySource("classpath:application-test.properties")
+class MqttBrokerTestConfig {
+
+  @Bean
+  InetSocketAddress externalNetworkAddress(
+      @Qualifier("externalNetwork") ServerNetwork<MqttConnection> externalNetwork,
+      @Qualifier("externalConnectionConsumer") Consumer<MqttConnection> externalConnectionConsumer) {
+    def address = externalNetwork.start()
+    externalNetwork.onAccept(externalConnectionConsumer)
+    return address
+  }
+
+  @Bean
+  InetSocketAddress internalNetworkAddress(
+      @Qualifier("internalNetwork") ServerNetwork<MqttConnection> internalNetwork,
+      @Qualifier("internalConnectionConsumer") Consumer<MqttConnection> internalConnectionConsumer) {
+    def address = internalNetwork.start()
+    internalNetwork.onAccept(internalConnectionConsumer)
+    return address
+  }
+}
---
+++ application/src/test/groovy/javasabr/mqtt/application/network/in/PublishReceivedInPacketTest.groovy
@@ -0,0 +1,72 @@
+package javasabr.mqtt.application.network.in
+
+import javasabr.mqtt.network.packet.in.PublishReceivedInPacket
+import javasabr.mqtt.model.PacketProperty
+import javasabr.mqtt.model.reason.code.PublishReceivedReasonCode
+import javasabr.rlib.collections.array.Array
+import javasabr.rlib.common.util.BufferUtils
+
+class PublishReceivedInPacketTest extends BaseInPacketTest {
+
+  def "should read packet correctly as mqtt 3.1.1"() {
+
+    given:
+
+        def dataBuffer = BufferUtils.prepareBuffer(512) {
+          it.putShort(packetId)
+        }
+
+    when:
+        def packet = new PublishReceivedInPacket(0b0101_0000 as byte)
+        def result = packet.read(mqtt311Connection, dataBuffer, dataBuffer.limit())
+    then:
+        result
+        packet.reason == ""
+        packet.packetId == packetId
+        packet.reasonCode == PublishReceivedReasonCode.SUCCESS
+        packet.userProperties == Array.empty()
+  }
+
+  def "should read packet correctly as mqtt 5.0"() {
+
+    given:
+
+        def propertiesBuffer = BufferUtils.prepareBuffer(512) {
+          it.putProperty(PacketProperty.REASON_STRING, reasonString)
+          it.putProperty(PacketProperty.USER_PROPERTY, userProperties)
+        }
+
+        def dataBuffer = BufferUtils.prepareBuffer(512) {
+          it.putShort(packetId)
+          it.put(PublishReceivedReasonCode.QUOTA_EXCEEDED.value)
+          it.putMbi(propertiesBuffer.limit())
+          it.put(propertiesBuffer)
+        }
+
+    when:
+        def packet = new PublishReceivedInPacket(0b0101_0000 as byte)
+        def result = packet.read(mqtt5Connection, dataBuffer, dataBuffer.limit())
+    then:
+        result
+        packet.reason == reasonString
+        packet.packetId == packetId
+        packet.reasonCode == PublishReceivedReasonCode.QUOTA_EXCEEDED
+        packet.userProperties == userProperties
+    when:
+
+        dataBuffer = BufferUtils.prepareBuffer(512) {
+          it.putShort(packetId)
+          it.put(PublishReceivedReasonCode.IMPLEMENTATION_SPECIFIC_ERROR.value)
+          it.putMbi(0)
+        }
+
+        packet = new PublishReceivedInPacket(0b0101_0000 as byte)
+        result = packet.read(mqtt5Connection, dataBuffer, dataBuffer.limit())
+    then:
+        result
+        packet.reason == ""
+        packet.packetId == packetId
+        packet.reasonCode == PublishReceivedReasonCode.IMPLEMENTATION_SPECIFIC_ERROR
+        packet.userProperties == Array.empty()
+  }
+}
