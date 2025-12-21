d6b201a9cdc44f0a2b41f0ec91c2af7ea2de2764
javasabr <javasabr@gmail.com>
2025-12-21 12:12:57 +0100

    preparing for PR

--- application/src/test/groovy/javasabr/mqtt/broker/application/IntegrationSpecification.groovy
+++ application/src/test/groovy/javasabr/mqtt/broker/application/IntegrationSpecification.groovy
@@ -12,7 +12,6 @@ import javasabr.mqtt.network.MqttConnection
 import javasabr.mqtt.network.MqttMockClient
 import javasabr.mqtt.network.user.ConfigurableNetworkMqttUser
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import spock.lang.Specification
